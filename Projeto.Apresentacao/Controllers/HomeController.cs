8aa1f5b4a1b6708b4329912a8981e8b31be76104
marcopinheiromarques <marco.pinheiro2@gmail.com>
2018-09-01 17:40:35 -0700

    Inicio da Autenticação do Projeto

--- Projeto.Apresentacao/Controllers/HomeController.cs
+++ Projeto.Apresentacao/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Projeto.Apresentacao.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -25,16 +26,6 @@ namespace Projeto.Apresentacao.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        public ActionResult Register()
-        {
-            return View();
-        }
+        }        
     }
 }
---
+++ Projeto.Apresentacao/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto.Apresentacao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult EsqueceuSenha()
+        {
+            return View();
+        }
+    }
+}
